a2f656493e138ecda87da8e1d1471f77d878ff45
cgupta3131 <cgupta3131@gmail.com>
2019-01-08 21:59:45 +0530

    JAN LONG

--- Jan2019_Long/Fancy.cpp
+++ Jan2019_Long/Fancy.cpp
@@ -1,22 +1,66 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 
-int main()
+bool solve(string input)
 {
-	int t;
-	cin >> t;
+	int size = input.size();
+	//cout << size << endl;
 
-	while(t--)
+	if(size<3)
+		return false;
+
+	if(input[0] == 'n' && input[1] == 'o' && input[2] == 't' 
+		&& (input[3] == ' ' || input[3] == 0) )
+		return true;
+
+	if(size == 3)
+		return false;
+
+	if(input[size-3] == 'n' && input[size-2] == 'o' && input[size-1] == 't' 
+		&& input[size-4] == ' ')
+		return true;
+
+	
+
+	for(int i=1;i<size-3;i++)
 	{
-		string input;
-		cin >> input;
-		cout << input;
 
-
+		if(input[i-1] == ' ' && input[i] == 'n' && input[i+1] == 'o' 
+			&& input[i+2] == 't' && input[i+3] == ' ')
+			return true;
 	}
 
+	return false;
+
+
+
+}
+
+int main()
+{	
+
+    int t;
+	cin >> t;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	for(int i=0;i<t;i++)
+	{ 
+		string input;
+		getline(cin,input);
+		//cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		//string input = helper;
+		//cout << "Input : " << input << endl;
+
+		/*  char c;
+		cin >> c;  */
+
+		if(solve(input) == true)
+			cout << "Real Fancy" << endl;
+		else
+			cout << "regularly fancy" << endl;
+
+	}
 
 	return 0 ; 
 
